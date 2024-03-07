d06349b0698cd5db80c537454580ea6034adca49
Aayush stha <sudipk339@gmail.com>
2024-03-07 21:16:46 +0545

    Merge Sort

--- day10.py
+++ day10.py
@@ -1,7 +1,34 @@
-import time
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
 
-while True:
-    current_time = time.localtime()
-    formatted_time = time.strftime("%H:%M:%S", current_time)
-    print("\r" + formatted_time, end="", flush=True)
-    time.sleep(1)
+    mid = len(arr) // 2
+    leftHalf = arr[:mid]
+    rightHalf = arr[mid:]
+
+    sortedLeft = mergeSort(leftHalf)
+    sortedRight = mergeSort(rightHalf)
+
+    return merge(sortedLeft, sortedRight)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+unsortedArr = [3, 7, 6, -10, 15, 23.5, 55, -13]
+sortedArr = mergeSort(unsortedArr)
+print("Sorted array:", sortedArr)
+
